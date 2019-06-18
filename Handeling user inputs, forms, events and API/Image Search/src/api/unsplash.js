6f03ad753c283ff11bbfcd20d2817d7c5e705465
Aditya Anand <38236391+adityaanand13@users.noreply.github.com>
2019-06-18 22:44:39 +0530

    Update unsplash.js

--- Handeling user inputs, forms, events and API/Image Search/src/api/unsplash.js
+++ Handeling user inputs, forms, events and API/Image Search/src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: "https://api.unsplash.com",
      headers: {
          Authorization:
-             'Client-ID 021c0145a9c3bd6f511c6f981621bb5ce058d76c556c98a4cad210a68bef8008'
+             'Client-ID ***************GET YOUR OWN KEY***************'
      }
  });
